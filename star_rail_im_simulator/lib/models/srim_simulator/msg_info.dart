2b6190a4f8b0154ddac9a6d9402f5f67dce5793f
Oyasuminasai <61616918+nfnfgo@users.noreply.github.com>
2023-05-10 00:54:42 +0800

    WorkOn: Base class for message info and message tile

--- star_rail_im_simulator/lib/models/srim_simulator/msg_info.dart
+++ star_rail_im_simulator/lib/models/srim_simulator/msg_info.dart
@@ -4,7 +4,120 @@ import 'dart:convert';
 // Models (Internal)
 import './srim_simulator.dart';
 
+/// An enum class which is used for mark the type of the SRIMMessageInfo
+class SRIMMessageType {
+  /// A text message type, usually sent by different characters in the game.
+  static const SRIMMessageType text = SRIMMessageType('text');
+
+  /// A photo message type, containing a picture as the message content.
+  static const SRIMMessageType photo = SRIMMessageType('photo');
+
+  /// An emoji message type, with an emoji as the message content.
+  static const SRIMMessageType emoji = SRIMMessageType('emoji');
+
+  /// A notice message type, indicating a system announcement or a conversation divider line, etc.
+  static const SRIMMessageType notice = SRIMMessageType('notice');
+
+  /// An unknown type message type, which means this message has an uncertain type,
+  /// or the program can NOT determine it's type.
+  static const SRIMMessageType unknown = SRIMMessageType('unknown');
+
+  /// The String object that represent the name of this message type
+  final String typeText;
+
+  /// Create a SRIMMessageType object from a `typeText`
+  const SRIMMessageType(this.typeText);
+
+  @override
+  String toString() {
+    return typeText;
+  }
+
+  /// A list contains all the initialized type of messages
+  static List<SRIMMessageType> get allTypeList {
+    return [
+      text,
+      photo,
+      emoji,
+      notice,
+      unknown,
+    ];
+  }
+
+  /// Returns a [SRIMMessageType] object based on received `typeText` param
+  ///
+  /// Throw `MsgTypeNotFound` Exception if could not find a type with the received
+  /// `typeText`. `unknown` is also allowed to be a `typeText` here
+  factory SRIMMessageType.fromString(String typeText) {
+    for (SRIMMessageType type in allTypeList) {
+      if (type.typeText == typeText) {
+        return type;
+      }
+    }
+    // if not found any matched type
+    throw Exception('[MsgTypeNotFound] Could not found a message type with '
+        'typeText "$typeText"');
+  }
+}
+
+/// Base model of message info
+class SRIMMsgInfoBase {
+  /// The type of the msgType
+  SRIMMessageType msgType;
+
+  ///Create a message info base class instance
+  SRIMMsgInfoBase({
+    this.msgType = SRIMMessageType.unknown,
+  });
+
+  /// Update the message information from a map object.
+  ///
+  /// Note: The subclass must call `super.fromMap()` to ensure that all the
+  /// information has been properly updated.
+  void fromMap(Map infoMap) {
+    // msgType
+    msgType = infoMap['msgType'] ?? msgType;
+  }
+
+  /// Converts the message object into a map object.
+  ///
+  /// Notice: The subclass usually need to call `super.toMap()` to get the map info
+  /// from super class
+  Map toMap() {
+    // init map
+    Map infoMap = {};
+    // msgType
+    infoMap['msgType'] = msgType.toString();
+    // return map
+    return infoMap;
+  }
+
+  /// Update this message info from another same message info object
+  void copyWith(SRIMMsgInfoBase anoMsgInfo) {
+    // msgType
+    msgType = anoMsgInfo.msgType;
+  }
+
+  /// Create a new copy of a already exist message info instance
+  ///
+  /// Notice: In most cases, this method NEED NOT to be overriden, you just need
+  /// to make sure the void `copyWith()` method of the subclass has been correctly
+  /// overriden, and this factory method will automatically call the `copyWith()` method
+  /// of the subclass.
+  ///
+  /// This is because in Dart, even if this instance has a literal
+  /// type of the base class, when you call `copyWith()` method, it will still
+  /// automatically check the runtime type and call the `copyWith()` method in the
+  /// subclass but not based on the type when you declare this class
+  factory SRIMMsgInfoBase.copyWith(SRIMMsgInfoBase anoMsgInfo) {
+    SRIMMsgInfoBase newMsgInfo = SRIMMsgInfoBase();
+    newMsgInfo.copyWith(anoMsgInfo);
+    return newMsgInfo;
+  }
+}
+
 class SRIMMsgInfo {
+  /// Creates a message info based on character info, sentBySelf info and msg content
   SRIMMsgInfo({
     this.characterInfo,
     this.sentBySelf = false,
@@ -13,10 +126,13 @@ class SRIMMsgInfo {
     characterInfo ??= SRIMCharacterInfo();
   }
 
+  /// The character information of the message sender.
   SRIMCharacterInfo? characterInfo;
 
+  /// Determines the display position and style of the message if it is sent by the user.
   bool sentBySelf;
 
+  /// The text content of the message.
   String msg;
 
   /// Create a new SRIMMsgInfo instance from another SRIMMsgInfo instance
